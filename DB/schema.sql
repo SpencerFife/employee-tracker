67efff03f910ed1a41dd5caa35caaee514af4b71
Spencer Fife <scottyfife17@gmail.com>
2020-05-17 12:17:32 -0700

    created DB connection functions, edited schema

--- DB/schema.sql
+++ DB/schema.sql
@@ -6,13 +6,13 @@ USE employeetracker_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
-    dep_name varchar(30) NULL,
+    name VARCHAR (30) NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
-    title varchar(30) NULL,
+    title VARCHAR (30) NULL,
     salary DECIMAL (10,4) NULL,
     department_id INT (10) NOT NULL,
     PRIMARY KEY (id)
@@ -20,8 +20,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
-    first_name varchar(30) NULL,
-    last_name varchar(30) NULL,
+    first_name VARCHAR (30) NULL,
+    last_name VARCHAR (30) NULL,
     role_id INT (30) NOT NULL,
     manager_id INT (30) NULL,
     PRIMARY KEY (id)
